d90d830324be692e1e7cb35f3f675f64eb25ce09
Subhashis Hansda <subhashishansda2@gmail.com>
2022-03-17 22:05:30 +0530

    added python file for data analysis

--- drug.py
+++ drug.py
@@ -5,4 +5,37 @@ Created on Mon Mar 14 12:50:12 2022
 @author: VAGUE
 """
 
-print("hello world")
+'''
+"John S. Delaney" - ESOL: Estimating Aqueous Solubility Directly from Molecular Structure (research paper)
+linear regression model for predicting molecular solubility
+
+"Pat Walters" - Deep Learning for the Life Sciences (author)
+'''
+
+# importing libraries
+import pandas as pd
+# CHEMBL database
+from chembl_webresource_client.new_client import new_client
+
+# search for target protein
+target = new_client.target
+target_query = target.search('coronavirus')
+targets = pd.DataFrame().from_dict(target_query)
+
+# select and retreive bioactivity data for SARS coronavirus 3C-like proteinase
+selected_target = targets.target_chembl_id[4]
+
+# filtering by selected_target and standard_type
+activity = new_client.activity
+res = activity.filter(target_chembl_id = selected_target).filter(standard_type = 'IC50')
+
+# creating dataframe
+df = pd.DataFrame().from_dict(res)
+df.head(3)
+df.standard_type.unique()
+
+# dataframe to csv
+df.to_csv('bioactivity_data.csv', index = False)
+
+# dropping missing value for the standard_value column
+df = df[df.standard_value.notna()]
