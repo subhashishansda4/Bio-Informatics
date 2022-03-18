babb743ea39f198daff1d97bf84be74e056ca1f0
Subhashis Hansda <subhashishansda2@gmail.com>
2022-03-18 16:41:51 +0530

    done with data collection

--- drug.py
+++ drug.py
@@ -12,6 +12,8 @@ linear regression model for predicting molecular solubility
 "Pat Walters" - Deep Learning for the Life Sciences (author)
 '''
 
+# DATA COLLECTION
+#---------------------------------------------------------------------------------------------------------
 # importing libraries
 import pandas as pd
 # CHEMBL database
@@ -39,3 +41,29 @@ df.to_csv('bioactivity_data.csv', index = False)
 
 # dropping missing value for the standard_value column
 df = df[df.standard_value.notna()]
+
+# data pre-processing
+# labelling compound as either being active, inactive or intermediate
+bioactivity_class = []
+for i in df.standard_value:
+    if float(i) >= 10000:
+        bioactivity_class.append('inactive')
+    elif float(i) <= 1000:
+        bioactivity_class.append('active')
+    else:
+        bioactivity_class.append('intermediate')
+        
+# selecting different parameters to iterate through and make them unique
+selection = ['molecule_chembl_id', 'canonical_smiles', 'standard_value']
+df_filter = df[selection]
+# merging dataframes
+dfs = [df_filter, pd.Series(bioactivity_class)]
+pd.concat(dfs, axis=1)
+
+# dataframe to csv
+df_filter.to_csv('bioactivity_preprocessed_data.csv', index = False)
+#---------------------------------------------------------------------------------------------------------
+
+
+# EDA
+#---------------------------------------------------------------------------------------------------------
